50d0da7a615ab858d3c04087cb01c82529852dde
Donald Mellenbruch <hello@Fresh2.dev>
2023-03-09 00:00:00 -0600

    fix: always cast default values to expected type

--- src/yapx/actions.py
+++ src/yapx/actions.py
@@ -1,7 +1,7 @@
 import collections.abc
 import shlex
 from enum import Enum
-from typing import Any, Dict, List, Optional, Set, Tuple, Type, TypeVar, Union
+from typing import Any, Dict, List, Optional, Set, Tuple, Type, TypeVar
 
 from .argparse_action import YapxAction, argparse_action
 from .types import ArgumentParser, ArgValueType
@@ -25,17 +25,8 @@ T = TypeVar("T", bound=type)
 
 def _split_csv_sequence(
     values: ArgValueType,
-    target_type: T,
-) -> Union[None, T, List[T]]:
-    def _cast_type(txt: str, target_type: T) -> T:
-        return (
-            txt
-            if is_instance(txt, target_type)
-            else (
-                target_type[txt] if is_subclass(target_type, Enum) else target_type(txt)
-            )
-        )
-
+    target_type: Optional[T],
+) -> Optional[List[T]]:
     if isinstance(values, str) and values:
         values = [values]
     elif (
@@ -70,31 +61,33 @@ def _split_csv_sequence(
                     target_type=target_type,
                 ),
             )
+        elif target_type and not is_instance(value, target_type):
+            all_values.append(
+                (
+                    target_type[value]
+                    if is_subclass(target_type, Enum)
+                    else target_type(value)
+                ),
+            )
         else:
-            all_values.append(_cast_type(value, target_type=target_type))
+            all_values.append(value)
 
     return all_values
 
 
-def _get_target_type(action: YapxAction, parser: ArgumentParser) -> type:
-    if not parser:
-        return action.type
-    # pylint: disable=protected-access
-    return parser._inner_type_conversions.get(action.dest, action.type)
-
-
 @argparse_action
-# pylint: disable=unused-argument
 def split_csv(
     values: ArgValueType,
     *,
     action: YapxAction,
     parser: ArgumentParser,
-    **kwargs: Any,
+    **_kwargs: Any,
 ) -> Optional[List[Optional[Any]]]:
     return _split_csv_sequence(
         values,
-        target_type=_get_target_type(action, parser),
+        target_type=parser._inner_type_conversions.get(  # pylint: disable=protected-access
+            action.dest,
+        ),
     )
 
 
@@ -104,11 +97,13 @@ def split_csv_to_tuple(
     *,
     action: YapxAction,
     parser: ArgumentParser,
-    **kwargs: Any,
+    **_kwargs: Any,
 ) -> Optional[Tuple[Optional[Any], ...]]:
     split_values: Optional[List[Optional[Any]]] = _split_csv_sequence(
         values,
-        target_type=_get_target_type(action, parser),
+        target_type=parser._inner_type_conversions.get(  # pylint: disable=protected-access
+            action.dest,
+        ),
     )
     if split_values is not None:
         return tuple(split_values)
@@ -121,11 +116,13 @@ def split_csv_to_set(
     *,
     action: YapxAction,
     parser: ArgumentParser,
-    **kwargs: Any,
+    **_kwargs: Any,
 ) -> Optional[Set[Optional[Any]]]:
     split_values: Optional[List[Optional[Any]]] = _split_csv_sequence(
         values,
-        target_type=_get_target_type(action, parser),
+        target_type=parser._inner_type_conversions.get(  # pylint: disable=protected-access
+            action.dest,
+        ),
     )
     if split_values is not None:
         return set(split_values)
@@ -138,7 +135,7 @@ def split_csv_to_dict(
     *,
     action: YapxAction,
     parser: ArgumentParser,
-    **kwargs: Any,
+    **_kwargs: Any,
 ) -> Optional[Dict[str, Optional[Any]]]:
     split_values: Optional[List[Optional[Any]]] = _split_csv_sequence(
         values,
@@ -165,12 +162,16 @@ def str2enum(
     *,
     action: YapxAction,
     parser: ArgumentParser,
-    **kwargs: Any,
+    **_kwargs: Any,
 ) -> Optional[Enum]:
     if values is None:
         return None
 
-    target_type: Type[Enum] = _get_target_type(action=action, parser=parser)
+    target_type: Type[Enum] = (
+        parser._inner_type_conversions.get(  # pylint: disable=protected-access
+            action.dest,
+        )
+    )
     if is_instance(values, target_type):
         return values
 
