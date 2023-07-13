031f5403e46cd68f70fc3a9fecd8aa14aba75b14
Donald Mellenbruch <hello@Fresh2.dev>
2023-07-13 00:00:00 -0500

    fix: casting of Dict value types

--- src/yapx/actions.py
+++ src/yapx/actions.py
@@ -6,7 +6,7 @@ from copy import copy
 from typing import Any, Dict, List, Optional, Sequence, Type, TypeVar
 
 from .types import ArgumentParser, ArgValueType
-from .utils import coalesce, try_issubclass
+from .utils import coalesce, try_isinstance
 
 
 class BooleanOptionalAction(Action):
@@ -184,7 +184,7 @@ def _split_csv_sequence(
         values = [values]
     elif (
         values is None
-        or not try_issubclass(type(values), collections.abc.Sequence)
+        or not try_isinstance(values, collections.abc.Sequence)
         or len(values) == 0
         or not isinstance(values[0], str)
     ):
@@ -195,6 +195,7 @@ def _split_csv_sequence(
     list_prefix: str = "["
     list_suffix: str = "]"
 
+    # if all (ignoring last) end with comma, remove comma.
     if all(x.endswith(",") for x in values[:-1]):
         values = [x.rstrip(",") for x in values]
 
@@ -286,33 +287,37 @@ class SplitCsvSetAction(_AppendAction):
 
 class SplitCsvDictAction(_AppendAction):
     def __call__(self, parser, namespace, values, option_string=None):
-        target_type: Optional[
-            Type[Any]
-        ] = parser._dest_type.get(  # pylint: disable=protected-access
-            self.dest,
-        )
-        split_values: Optional[List[str]] = _split_csv_sequence(
-            values,
-            target_type=target_type,
-        )
-
         split_values_dict: Optional[Dict[str, Any]] = None
-        if split_values is not None:
-            split_values_dict = {
-                x_split[0].strip(): (
-                    v if v is None or target_type is None else target_type(v)
-                )
-                for x in split_values
-                if x
-                for x_split in [x.split(parser.kv_separator, maxsplit=1)]
-                for v in [
-                    (
-                        None
-                        if len(x_split) < 2
-                        else coalesce(x_split[1].strip(), None, null_or_empty=True)
-                    ),
-                ]
-            }
+
+        if not values or isinstance(values, dict):
+            split_values_dict = values
+        else:
+            split_values: Optional[List[str]] = _split_csv_sequence(
+                values,
+                target_type=str,
+            )
+            if split_values:
+                split_values_dict = {
+                    x_split[0].strip(): v
+                    for x in split_values
+                    if x
+                    for x_split in [x.split(parser.kv_separator, maxsplit=1)]
+                    for v in [
+                        (
+                            None
+                            if len(x_split) < 2
+                            else coalesce(x_split[1].strip(), None, null_or_empty=True)
+                        ),
+                    ]
+                }
+
+        # pylint: disable=protected-access
+        if split_values_dict and self.dest in parser._dest_type:
+            target_type: Type[Any] = parser._dest_type[self.dest]
+            if target_type:
+                split_values_dict = {
+                    k: target_type(v) for k, v in split_values_dict.items()
+                }
 
         items = _append_new_dest_values(
             namespace,
